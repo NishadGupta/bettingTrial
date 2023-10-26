647f3c6c5b6b2b04a52a48ae22741a658776f370
guptanis <guptanis@iu.edu>
2023-10-25 21:17:34 -0400

    Wish me luck the second time

--- app.py
+++ app.py
@@ -481,7 +481,7 @@ def place_bets(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host = '0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
